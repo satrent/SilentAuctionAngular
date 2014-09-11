cd6bf5909968dae7c8414dc31032f4787c4b2638
Evan Duncan <evan.e.duncan@gmail.com>
2014-09-10 20:22:17 -0500

    sanitizing email address as well for consistency

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -73,7 +73,7 @@ silentAuctionControllers.controller('RegisterController', ['$scope', '$http', '$
     var user = {
       userName: $scope.userData.userName.toLowerCase(),
       password: $scope.userData.password,
-      email: $scope.userData.userName + '@porticobenefits.org',
+      email: $scope.userData.userName.toLowerCase() + '@porticobenefits.org',
       isAdmin: false
     };
 
